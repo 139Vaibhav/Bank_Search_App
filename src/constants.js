dd9ff78f5fdb73f4c56d492fb75d1cf3063dbf8a
139Vaibhav <vaibhavkumarsingh139@gmail.com>
2021-04-10 17:43:32 +0530

    Bank Search App

---
+++ src/constants.js
@@ -0,0 +1,9 @@
+export const options = [
+  { label: "Delhi", value: "DELHI" },
+  { label: "Mumbai", value: "MUMBAI" },
+  { label: "Hyderabad", value: "HYDERABAD" },
+  { label: "Chennai", value: "CHENNAI" },
+  { label: "Bangalore", value: "BANGALORE" }
+];
+
+export const defaultDropdownValue = { label: "Mumbai", value: "MUMBAI" };
